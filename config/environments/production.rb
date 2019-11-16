f819427f0452aea482d014ec4801c1236bc04d81
hamotaro <tetsuya1329@gmail.com>
2019-11-16 19:54:45 +0900

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  #デプロイ時に背景イメージを読み込む
+  config.assets.compile = true
+  config.serve_static_assets = true
 end
